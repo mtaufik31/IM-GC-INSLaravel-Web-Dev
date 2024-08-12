fde17449155a52e6972f5bed9e6f2fe02e616338
mtaufik31 <taufikbudiman031@gmail.com>
2024-08-12 13:23:04 +0700

    JS DOM

--- Hari-5 Muhammad Taufik B/4.Looping/script.js
+++ Hari-5 Muhammad Taufik B/4.Looping/script.js
@@ -3,14 +3,12 @@ var jawabanContoh = "";
       for (var i = 0; i < 6; i++) {
         jawabanContoh += "ini adalah angka ke: " + [i] + "<br>";
       }
-      // Driver Code
       document.getElementById("example").innerHTML = jawabanContoh;
 
       // Soal No. 1 Looping menggunakan While
 
       var jawaban1 = "";
 
-      // Looping Pertama (Maju)
       var i = 2;
       jawaban1 += "LOOPING PERTAMA<br>";
       while (i <= 20) {
@@ -18,26 +16,24 @@ var jawabanContoh = "";
         i += 2;
       }
       /*
-      
-
-Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while. Untuk membuat tantangan ini lebih menarik,kamu juga diminta untuk membuat suatu looping yang menghitung maju dan menghitung mundur. OUTPUT : LOOPING PERTAMA
-2 - I love coding
-4 - I love coding
-6 - I love coding
-8 - I love coding
-10 - I love coding
-12 - I love coding
-14 - I love coding
-16 - I love coding
-18 - I love coding
-20 - I love coding
-LOOPING KEDUA
-20 - I will become web developer
-18 - I will become web developer
-16 - I will become web developer
-14 - I will become web developer
-12 - I will become web developer
-10 - I will become web developer 8 - I will become web developer 6 - I will become web developer 4 - I will become web developer 2 - I will become web developer */
+      Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while. Untuk membuat tantangan ini lebih menarik,kamu juga diminta untuk membuat suatu looping yang menghitung maju dan menghitung mundur. OUTPUT : LOOPING PERTAMA
+      2 - I love coding
+      4 - I love coding
+      6 - I love coding
+      8 - I love coding
+      10 - I love coding
+      12 - I love coding
+      14 - I love coding
+      16 - I love coding
+      18 - I love coding
+      20 - I love coding
+      LOOPING KEDUA
+      20 - I will become web developer
+      18 - I will become web developer
+      16 - I will become web developer
+      14 - I will become web developer
+      12 - I will become web developer
+      10 - I will become web developer 8 - I will become web developer 6 - I will become web developer 4 - I will become web developer 2 - I will become web developer */
 
       i = 20;
       jawaban1 += "LOOPING KEDUA<br>";
